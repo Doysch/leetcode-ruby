42e523bb0e5b23b1cf43b272e301916c3dbed063
David Bacon <dpb@davidbacon.co.uk>
2022-06-30 13:47:52 +0100

    tidied up two sum code

--- two_sum.rb
+++ two_sum.rb
@@ -1,16 +1,16 @@
 # 🤓 challenge here https://leetcode.com/problems/two-sum/
 
-# def two_sum(nums, target)
-#   arr =[]
-#   nums.each_with_index do |num, i|
-#     nums.each_with_index do |num, j|
-#       if (nums[i] + nums[j] == target) && j > i
-#         return [i, j]
-#       end
-#     end
-#     arr
-#   end
-# end
+def two_sum(nums, target)
+  arr = []
+  nums.each_with_index do |num, i|
+    nums.each_with_index do |num, j|
+      if (nums[i] + nums[j] == target) && j > i
+        return [i, j]
+      end
+    end
+    arr
+  end
+end
 
 # 1. Iterate through the nums array with index, against the same array with index
 # 2. Check whether the sum of two of the elements add up to target
