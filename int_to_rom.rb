4f6beb35006c0527778585bcd6747e62154f144e
David Bacon <dpb@davidbacon.co.uk>
2022-07-07 13:20:34 +0100

    tidied up code, made hash a constant to make method shorter

--- int_to_rom.rb
+++ int_to_rom.rb
@@ -1,28 +1,24 @@
-def run(n)
-    roman_numbers = {
-1000 => "M",
-900 => "CM",
-500 => "D",
-400 => "CD",
-100 => "C",
-  90 => "XC",
-  50 => "L",
-  40 => "XL",
-  10 => "X",
-    9 => "IX",
-    5 => "V",
-    4 => "IV",
-    1 => "I",
-}
-roman = ""
-roman_numbers.each do |value, letter|
-  roman << letter*(n / value)
-  n = n % value
+def run(num)
+  roman = ''
+  ROMAN_NUMERALS.each do |value, letter|
+    roman << letter * (num / value)
+    num = num % value
   end
-roman
-
-#     n_in_roman_alphabet = nil
-# return n_in_roman_alphabet
+  roman
 end
 
-p run(3500)
+ROMAN_NUMERALS = {
+  1000 => 'M',
+  900 => 'CM',
+  500 => 'D',
+  400 => 'CD',
+  100 => 'C',
+  90 => 'XC',
+  50 => 'L',
+  40 => 'XL',
+  10 => 'X',
+  9 => 'IX',
+  5 => 'V',
+  4 => 'IV',
+  1 => 'I'
+}
